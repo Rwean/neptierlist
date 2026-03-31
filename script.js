b951028fafb20de26f0b09fb68a92fc6885add49
Rwean <ibrahimefekilic5@gmail.com>
2026-03-31 16:19:44 +0300

    neptıerlistmc

---
+++ script.js
@@ -0,0 +1,6 @@
+   const buton = document.getElementById('renkButonu');
+
+buton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 
+        "#" + Math.floor(Math.random()*16777215).toString(16);
+}); 
